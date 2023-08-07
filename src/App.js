71ebdf364aa532ffe08094f8e2102d351c44cc83
aneesh_augustine <aneeshaugustine911@gmail.com>
2023-08-07 19:01:35 +0530

    movie posters added dinamicaly

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from "react";
 import NavBar from "./Components/NavBar/NavBar";
 import Banner from "./Components/Banner/Banner";
 import RowPost from "./Components/RowPost/RowPost";
- function App() {
+import {ActionMovies,Originals,Documentaries,RomanceMovies,HorrorMovies,ComedyMovies,trending} from "./urls";
+function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <Banner/>
-      <RowPost/>
+      <NavBar />
+      <Banner />
+      <RowPost url={Originals} title="Netfilx Originals"/>
+      <RowPost url={trending} title="Trending"/>
+      <RowPost url={ActionMovies} title="Action" isSmall />
+      <RowPost url={RomanceMovies} title="Romance" isSmall />
+      <RowPost url={ComedyMovies} title="Comedy" isSmall />
+      <RowPost url={HorrorMovies} title="Horror" isSmall />
+      <RowPost url={Documentaries} title="Documentaries" isSmall />
     </div>
   );
 }
--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,28 +1,31 @@
-import React from 'react'
-import './RowPost.css'
+import React, { useEffect, useState } from "react";
+import "./RowPost.css";
+import {imageUrl } from "../../constance/constance";
+import axios from "../../constance/axios";
 
-function RowPost() {
+function RowPost(props) {
+  const [movie, setMovies] = useState([]);
+  useEffect(() => {
+    axios
+      .get(props.url)
+      .then((response) => {
+        console.log(response.data);
+        setMovies(response.data.results);
+      })
+      .catch((err) => {
+        //alert('network error')
+      });
+  }, []);
   return (
-    <div className='row'>
-        <h2>Title</h2>
-        <div className="posters">
-            <img className='poster' src="./mv_banner.jpg" alt="poster"/>
-            <img className='poster' src="./mv_banner.jpg" alt="poster"/>
-            <img className='poster' src="./mv_banner.jpg" alt="poster"/>
-            <img className='poster' src="./mv_banner.jpg" alt="poster"/>
-            <img className='poster' src="./mv_banner.jpg" alt="poster"/>
-            <img className='poster' src="./mv_banner.jpg" alt="poster"/>
-            <img className='poster' src="./mv_banner.jpg" alt="poster"/>
-            <img className='poster' src="./mv_banner.jpg" alt="poster"/>
-            <img className='poster' src="./mv_banner.jpg" alt="poster"/>
-            <img className='poster' src="./mv_banner.jpg" alt="poster"/>
-            <img className='poster' src="./mv_banner.jpg" alt="poster"/>
-            <img className='poster' src="./mv_banner.jpg" alt="poster"/>
-            <img className='poster' src="./mv_banner.jpg" alt="poster"/>
-  
-        </div>
+    <div className="row">
+      <h2>{props.title}</h2>
+      <div className="posters">
+        {movie.map((obj) =>
+          <img className={props.isSmall ? "smallPosters":"poster"} src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
+        )}
+      </div>
     </div>
   )
 }
 
-export default RowPost
+export default RowPost;
